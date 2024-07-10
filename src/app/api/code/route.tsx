a5a4a51bcd2230fb5ad6af611769e669b1bc9cb6
Francisco Leal <francisco@talentprotocol.com>
2024-07-10 14:19:06 +0100

    feat: Add whitelisted addresses

--- src/app/api/code/route.tsx
+++ src/app/api/code/route.tsx
@@ -1,12 +1,23 @@
 import type { NextRequest } from "next/server";
 import { supabase } from "@/db";
 
+const WHITELISTED_ADDRESS = {
+  "0x33041027dd8f4dc82b6e825fb37adf8f15d44053": true,
+} as Record<string, boolean>;
+
 export async function GET(request: NextRequest) {
   const wallet = request.nextUrl.searchParams.get("owner") ?? undefined;
   const newSafeCode = crypto.randomUUID();
 
   if (!wallet) {
     return Response.json({ message: "Invalid wallet" }, { status: 400 });
+  }
+
+  if (!WHITELISTED_ADDRESS[wallet.toLowerCase()]) {
+    return Response.json(
+      { message: "You can not tip others" },
+      { status: 400 }
+    );
   }
 
   const { error } = await supabase
