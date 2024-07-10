00895a510666a0ae8f0dab41fb1f2d2e88d03d92
Francisco Leal <francisco@talentprotocol.com>
2024-07-10 15:05:17 +0100

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,28 @@ import "dotenv/config";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const description = "Tip $BUILD to real builders during IRL events";
+
 export const metadata: Metadata = {
   title: "Tip BUILD IRL",
-  description: "Nominate your frens IRL",
+  description: description,
   icons: ["/favicon.ico"],
+  openGraph: {
+    title: "BUILD",
+    description: description,
+    type: "website",
+    url: "https://tip.build.top",
+    images: [
+      "https://build-top-images.s3.eu-west-2.amazonaws.com/build-tipping.png",
+    ],
+  },
+  twitter: {
+    title: "BUILD",
+    description: description,
+    images: [
+      "https://build-top-images.s3.eu-west-2.amazonaws.com/build-tipping.png",
+    ],
+  },
 };
 
 export default function RootLayout({
